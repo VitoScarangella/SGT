b45423b1f319c6e733a37a509332a72a180e5220
VitoScarangella <vitoscarangella92@gmail.com>
2017-07-12 14:54:44 +0200

    label changed 2
    societa' -> società

--- sgt/views/sgt-societa/_take.php
+++ sgt/views/sgt-societa/_take.php
@@ -46,7 +46,7 @@ use common\models\Tool;
     [
 		'columns'=>2,
 		'attributes' => [
-            'societa'=>['label'=>'Nome della societa'','type'=> Form::INPUT_TEXT, 'options'=>['class'=>'textField','placeholder'=>'Società...', 'maxlength'=>150 ]],
+            'societa'=>['label'=>'Nome della società,'type'=> Form::INPUT_TEXT, 'options'=>['class'=>'textField','placeholder'=>'Società...', 'maxlength'=>150 ]],
             'email'=>['label'=>'','type'=> Form::INPUT_TEXT, 'options'=>['class'=>'textField','type'=>'email', 'placeholder'=>'Inserisci una mail che usi spesso sulla quale desideri ricevere le nostre comunicazioni', 'maxlength'=>30]],
 			]
 		],
